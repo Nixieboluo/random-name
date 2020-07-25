fda7ddd2fb2653741c2b290759cd91d67a9b8c7a
Nixieboluo <baka_boluo@outlook.com>
2020-07-25 10:38:52 +0800

    Initial commit.

---
+++ src/components/List.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="container mx-auto items-center mt-32 mb-32">
+    <div
+      class="grid gap-4 mx-6 grid-cols-2 sm:grid-cols-4 md:grid-cols-5 lg:grid-cols-6 xl:grid-cols-8"
+      id="list"
+    >
+      <!-- 队列显示区域 -->
+      <div
+        class="bg-gray-300 shadow-md rounded-full text-center text-gray-600 pt-3 w-full h-12"
+      >
+        Test
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "List"
+};
+</script>
