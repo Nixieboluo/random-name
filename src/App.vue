6da904acbe197403f1833ad44cc8ea3e2980b61f
Nixieboluo <baka_boluo@outlook.com>
2020-07-25 18:11:36 +0800

    Added basic structures.

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,16 @@
 <template>
   <div id="app">
+    <div
+      class="bg-teal-200 text-teal-600 text-lg text-center pt-12 w-64 h-32 mx-auto my-12"
+    >
+      Logo Placeholder
+    </div>
     <List></List>
+    <input
+      type="button"
+      value="Pick One"
+      class="bg-blue-500 hover:bg-blue-700 focus:outline-none focus:shadow-outline text-white font-bold block text-center rounded-none md:rounded fixed md:relative bottom-0 w-full md:w-1/2 md:m-auto h-12"
+    />
   </div>
 </template>
 
--- src/components/List.vue
+++ src/components/List.vue
@@ -1,14 +1,18 @@
 <template>
-  <div class="container mx-auto items-center mt-32 mb-32">
+  <div class="container mx-auto items-center mt-8 my-12">
     <div
-      class="grid gap-4 mx-6 grid-cols-2 sm:grid-cols-4 md:grid-cols-5 lg:grid-cols-6 xl:grid-cols-8"
-      id="list"
+      class="grid gap-4 mx-6 my-8 grid-cols-2 sm:grid-cols-4 md:grid-cols-5 lg:grid-cols-6 xl:grid-cols-8"
     >
       <!-- 队列显示区域 -->
       <div
-        class="bg-gray-300 shadow-md rounded-full text-center text-gray-600 pt-3 w-full h-12"
+        v-bind:class="{
+          'bg-red-200 text-red-600': item.active
+        }"
+        class="bg-gray-300 text-gray-600 shadow-md rounded-full text-center pt-3 w-full h-12"
+        v-for="item in list"
+        :key="item.name"
       >
-        Test
+        {{ item.name }} weights {{ item.weight }}
       </div>
     </div>
   </div>
@@ -16,6 +20,18 @@
 
 <script>
 export default {
-  name: "List"
+  name: "List",
+  data: function() {
+    return {
+      list: []
+    };
+  },
+  created() {
+    fetch("conf.json")
+      .then(response => response.json())
+      .then(json => {
+        this.list = json.list;
+      });
+  }
 };
 </script>
